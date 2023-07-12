c5ca5f3cb0d32e3a53374e5d75c811e2a188975c
ruescog <ruescog@unirioja.es>
2023-07-12 12:39:23 +0200

    first version

--- explainable_exceptions/_modidx.py
+++ explainable_exceptions/_modidx.py
@@ -5,4 +5,7 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://ruescog.github.io',
                 'git_url': 'https://github.com/ruescog/explainable-exceptions',
                 'lib_path': 'explainable_exceptions'},
-  'syms': {'explainable_exceptions.core': {'explainable_exceptions.core.foo': ('core.html#foo', 'explainable_exceptions/core.py')}}}
+  'syms': { 'explainable_exceptions.core': { 'explainable_exceptions.core.explain': ('core.html#explain', 'explainable_exceptions/core.py'),
+                                             'explainable_exceptions.core.explain_exception': ( 'core.html#explain_exception',
+                                                                                                'explainable_exceptions/core.py'),
+                                             'explainable_exceptions.core.login': ('core.html#login', 'explainable_exceptions/core.py')}}}
